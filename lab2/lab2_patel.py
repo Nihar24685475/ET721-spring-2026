27b2db59c8fccf1f0953da348b0cedaa9e91d941
nihar <Niharpatel850@gmail.com>
2026-01-29 12:59:26 -0500

    create testing2 branch

--- lab2/lab2_patel.py
+++ lab2/lab2_patel.py
@@ -9,3 +9,9 @@ print(f'\nWelcome to git commands {username}')
 #testing line
 num = 8
 print(f'Double of number {num} is {num*2}')
+
+#testing2 : calculate hypotenuse
+x=5
+y=3
+hyp = (x**2 + y**2)**0.5
+print (f'The hypotenuse of {x} and {y} is {hyp}')
