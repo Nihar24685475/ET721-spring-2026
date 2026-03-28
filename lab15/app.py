39b8642d233b7d42c44fd348ec3648c3eec6a09d
Niharpatel <Niharpatel850@gmail.com>
2026-03-28 00:37:59 -0400

    lab15 done

--- lab15/app.py
+++ lab15/app.py
@@ -33,16 +33,34 @@ def create_item():
 @app.route('/items',methods = ['GET'])
 def get_items():
     return jsonify(items)
-# READ SINGLE ITEM
-@app.route('/items/<item_id>',methods =['GET'])
-def get_item(item_id):
-    item = items.get(item_id)
-    if not item:
-        #404 = server is rechaable but the item you asked for doesn't exiest
-        return jsonify({'error':'Item not found'}), 404
-    
-    return jsonify(item)
 
+# READ, UPDATE SINGLE ITEM
+@app.route('/items/<item_id>', methods=['GET','PUT','DELETE'])
+def handle_item(item_id):
+    item = items.get(item_id)
+
+    if request.method == 'GET':
+        
+        if not item:
+            #404 = server is rechaable but the item you asked for doesn't exiest
+            return jsonify({'error':'Item not found'}), 404
+        return jsonify(item)
+    
+    #UPDATE 
+    elif request.method == 'PUT':
+        if not item:
+            return render_template('error.html',message = "Item not found"), 404
+        data = request.get_json()
+        if not data:
+            return jsonify({'error':'Invalid input'}), 400
+        items[item_id] = data
+        return render_template('update.html', item_id = item_id, item = data)
+    
+    #DELETE 
+    elif request.method == "DELETE":
+        deleted_item = items.pop(item_id)
+        return render_template('delete.html',item_id = item_id,deleted_item = deleted_item)
+    
 
 if __name__ == '__main__':
     app.run(debug=True)
